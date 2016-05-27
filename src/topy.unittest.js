3262763ea71a5ff312adceebeffb203238c5943c
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-05-27 11:36:42 -0400

    added a unit test function

---
+++ src/topy.unittest.js
@@ -0,0 +1,11 @@
+function unitTest() {
+	log('------------- unit test begins -------------')
+	var array = [2, 3, 4, 8, 19, 31];
+
+	log(array)
+	var array2 = removeFromArray(array, 3);
+
+	log(array)
+
+	log('------------- unit test ends -------------')
+}
