67f8f236d48b759322e8f12836de209492fad1c8
Xiang 'Anthony' Chen <xiangchen@cmu.edu>
2016-05-31 16:08:18 -0400

    added a potential set of methods for optimization

---
+++ src/topy.optimization.js
@@ -0,0 +1,12 @@
+//
+//	precomputing a tpd file for topology optimization
+//
+//	@param mesh - the initial volume to be optimized 
+//	(e.g., a connector made from a sphere with space carved out for the things it connects)
+//	@param boundaryVertices - vertices specified as boundary
+//	@param loadVertices - vertices specified as sustaining loads
+//	@param loadAmnts - amounts of load corresponding to loadVertices
+//
+function preCompTpd(mesh, boundaryVertices, loadVertices, loadAmnts) {
+	
+}
